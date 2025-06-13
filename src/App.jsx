228ef0fcd501b9c0662f7c00d795bd798c018e00
DreamWayAI <dreamway.agency01@gmail.com>
2025-06-13 18:19:01 +0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-white text-3xl">
-      DreamWay Admin Працює ✅
-    </div>
-  );
-}
